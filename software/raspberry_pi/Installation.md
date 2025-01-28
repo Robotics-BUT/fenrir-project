1c35b163ae9a9636ce8c5aaebe82ee3bf2673102
Jakub Minařík <203294@vut.cz>
2025-01-28 09:47:04 +0100

    Installation scripts

---
+++ software/raspberry_pi/Installation.md
@@ -0,0 +1,20 @@
+
+On first start run first_start.sh
+Script will:
+- run system update and upgrade some system packages
+- change boot firmware config
+- create swapfile
+- add user to groups - video, tty, dialout
+- disable boot ethernet timeout
+System needs to be rebooted after.
+After reboot run install.sh
+Script will:
+- update system and install packages
+- install python packages
+- install ROS 2 and colcon
+- clone this repository
+- build ROS2 packages
+- add services to system
+
+
+
---
+++ software/raspberry_pi/install.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+#Update system and install apt-get packages
+sudo apt-get update
+sudo apt-get install -y \
+	git vim raspi-config htop \
+	locales software-properties-common \
+	curl libopencv-dev build-essential \
+	wiringpi v4l-utils i2c-tools \
+	gstreamer1.0-tools gstreamer1.0-plugins-base \
+	gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+	gstreamer1.0-plugins-ugly
+
+#Python packages
+sudo apt-get install -y python3-pip
+sudo pip3 install rpi_ws281x \
+	adafruit-blink \
+	adafruit-circuitpython-neopixel \
+	adafruit-circuitpython-bme280 \
+	adafruit-circuitpython-ads1x15 \
+	adafruit-circuitpython-mpu6050 \
+	adafruit-circuitpython-ds3231 \
+	RPLCD smbus2 pynmea2
+
+
+
+##INSTALL ROS2	
+#configure locale
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+#Add ROS repository
+sudo add-apt-repository -y universe #enable universe repository
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg #add ros2 gpg key
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null #add repository to source list
+
+#Install ROS packages
+sudo apt-get update
+sudo apt-get install -y ros-humble-ros-base ros-humble-cv-bridge ros-humble-image-transport
+
+curl -s https://packagecloud.io/install/repositories/dirk-thomas/colcon/script.deb.sh | sudo bash
+sudo apt-get install -y python3-colcon-common-extensions
+
+source /opt/ros/humble/setup.bash
+
+#colcon
+curl -s https://packagecloud.io/install/repositories/dirk-thomas/colcon/script.deb.sh | sudo bash
+sudo apt-get install -y python3-colcon-common-extensions
+
+#Repo clone
+git clone --depth 1 https://github.com/Robotics-BUT/fenrir-project.git
+cd fenrir-project
+git submodule update --init --recursive
+
+cd /home/robot/fenrir-project/software/raspberry_pi/ros2_ws
+
+colcon build
+source /home/robot/fenrir-project/software/raspberry_pi/ros2_ws/install/setup.bash
+colcon build
+source /home/robot/fenrir-project/software/raspberry_pi/ros2_ws/install/setup.bash
+
+
+###adding service
+sudo cp sudo_node.service /etc/systemd/system/prp_root.service
+sudo cp user_node.servise /etc/systemd/system/prp_user.service
